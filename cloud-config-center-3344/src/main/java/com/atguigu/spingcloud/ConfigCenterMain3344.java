302517d63bd69a7337b3116851ee590ed6f45f3b
dhc <dhc0611@126.com>
2020-10-17 15:26:02 +0800

    SpringCloud Config  分布式配置中心 SpringCloud Bus     消息总线

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/spingcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.atguigu.spingcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author denghanchen
+ * @date 2020/10/16 16:28
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+
+}
