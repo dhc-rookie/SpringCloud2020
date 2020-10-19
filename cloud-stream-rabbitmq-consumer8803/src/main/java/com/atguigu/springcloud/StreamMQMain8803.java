b56d6f61ca51846920b3df9c7a36eb522fb34518
dhc <dhc0611@126.com>
2020-10-19 10:15:40 +0800

    SpringCloud Stream  消息驱动

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/atguigu/springcloud/StreamMQMain8803.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author denghanchen
+ * @date 2020/10/19 9:37
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/IMessageProvider.java
@@ -0,0 +1,10 @@
+package com.atguigu.springcloud.service;
+
+/**
+ * @author denghanchen
+ * @date 2020/10/19 9:21
+ */
+public interface IMessageProvider {
+
+    public String send();
+}
